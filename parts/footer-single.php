5a8e7252f8ddc5348c45fa187f87015fa568444c
Pablo Selin <pabloselin@gmail.com>
2022-07-21 11:31:12 +0200

    ajuste single tax

--- parts/footer-single.php
+++ parts/footer-single.php
@@ -3,7 +3,9 @@
     <div class="single-item-meta-bottom">
       <div class="single-tax-zone"><?php the_terms( $post->ID, 'areas', '<p><span class="taxname">áreas</span></p>', ', ' );?></div>
       <div class="single-tax-zone"><?php the_terms( $post->ID, 'ambitos_de_accion', '<p><span class="taxname ambitos-de-accion">ámbitos</span></p>', ', ' );?></div>
-      <div class="single-tax-zone"><?php the_terms( $post->ID, 'post_tag', '<p><span class="taxname">etiquetas</span></p>', ', ' );?> </div>
+      <div class="single-tax-zone"><?php the_terms( $post->ID, 'tema', '<p><span class="taxname tema">temas</span></p>', ', ' );?></div>
+      <div class="single-tax-zone"><?php the_terms( $post->ID, 'post_tag', '<p><span class="taxname tags">etiquetas</span></p>', ', ' );?> </div>
+      <div class="single-tax-zone"><?php the_terms( $post->ID, 'germina_year', '<p><span class="taxname germina_year">años</span></p>', ', ' );?> </div>
     </div>
   </footer>
 </div>
