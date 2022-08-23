02067e3d801c3bddecf64b88a563724357a0e369
Pablo Selin <pabloselin@gmail.com>
2022-08-23 09:47:03 +0200

    new video url

--- parts/video.php
+++ parts/video.php
@@ -1,6 +1,6 @@
 <div class="germina-video-container">
   <video class="video-germina video-js vjs-big-play-centered" controls preload="metadata" width="555" height="312" poster="<?php bloginfo('template_url');?>/assets/img/video_snap_01.png" data-setup='{"fluid": true, "aspectRatio": "16:9"}'>
-    <source src="<?php bloginfo('template_url');?>/assets/img/somos-germina.mp4" type="video/mp4">
+    <source src="https://germina.cl/wp-content/uploads/2022/08/GERMINA_2022.mp4" type="video/mp4">
     <p class="vjs-no-js">
       Se necesita un navegador compatible con HTML5 para ver este video.
     </p>
